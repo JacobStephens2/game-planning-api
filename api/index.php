a94d6fb2f4d5749ef4ba80cd3bac8e35d389d882
Jacob Stephens <jacob@stewardgoods.com>
2022-01-30 17:20:08 -0500

    move data.php workings to index.php

--- api/index.php
+++ api/index.php
@@ -1,5 +1,20 @@
 <?php
 
-echo '/api/index.php file';
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET");
+header("Content-Type: application/json; charset=UTF-8");
 
-// Silence
+class testObject {}
+
+$myObj = new testObject;
+
+$myObj->name = "Data from the index.php endpoint of the Game Planning App API";
+$myObj->endPointsDescription = "https://github.com/JacobStephens2/game-planning-back-end";
+$myObj->endPointBaseURL = "http://ec2-184-73-147-183.compute-1.amazonaws.com/back-end/api/";
+$myObj->endPointsList = array("index.php", "database-test.php");
+
+$myJSON = json_encode($myObj);
+
+echo $myJSON;
+
+?>
