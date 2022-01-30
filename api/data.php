4720c46b1c30f8dc8a28f0ce7f7d09ab44db0136
Jacob Stephens <jacob@stewardgoods.com>
2022-01-30 14:01:26 -0500

    change backend data from text to json

--- api/data.php
+++ api/data.php
@@ -4,7 +4,11 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET");
 header("Content-Type: application/json; charset=UTF-8");
 
-$a = 'Data from api.php';
+$a = 
+  '{
+    "name": "data from backend"
+  }';
+  
 echo ($a);
 
 ?>
