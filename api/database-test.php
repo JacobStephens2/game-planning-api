9e5adebde97a93fc30a8030b6ffb5fa8b8108974
Jacob Stephens <jacob@stewardgoods.com>
2022-01-30 17:19:49 -0500

    add header access control to database-test.php

--- api/database-test.php
+++ api/database-test.php
@@ -1,5 +1,7 @@
 <?php
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET");
 header("Content-Type: application/json; charset=UTF-8");
 
 require_once('../database/initialize.php');
