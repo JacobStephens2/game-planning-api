8cedbd7fb1401ade558a44b385eb8a33914dfa37
Jacob Stephens <jacob@stewardgoods.com>
2022-02-19 09:35:55 -0500

    change name to purpose on database-test result

--- database-test.php
+++ database-test.php
@@ -6,7 +6,7 @@ header("Content-Type: application/json; charset=UTF-8");
 
 require_once('database/initialize.php');
 
-$site_data = array("name"=>get_site_data());
+$site_data = array("purpose"=>get_site_data());
 
 echo json_encode($site_data);
 
