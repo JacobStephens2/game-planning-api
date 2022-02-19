3488348599ce45d3fddbbdf07ed0ad1afb02693f
Jacob Stephens <jacob@stewardgoods.com>
2022-02-19 09:35:37 -0500

    add game.php endpoint

---
+++ game.php
@@ -0,0 +1,13 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET");
+header("Content-Type: application/json; charset=UTF-8");
+
+require_once('database/initialize.php');
+
+$data = array("title"=>get_game());
+
+echo json_encode($data);
+
+?>
