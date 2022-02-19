03cd914b82d8f42a9eb536c5025eac616da7f678
Jacob Stephens <jacob@stewardgoods.com>
2022-02-19 09:35:29 -0500

    set Game class database

--- database/initialize.php
+++ database/initialize.php
@@ -5,4 +5,8 @@ include_once('database_functions.php');
 $database = db_connect();
 include_once('query_functions.php');
 
+require_once('classes/game.class.php');
+
+Game::set_database($database);
+
 ?>
