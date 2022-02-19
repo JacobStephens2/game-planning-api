90ed0906519e71e061ed3d6695194fddd0bc54bd
Jacob Stephens <jacob@stewardgoods.com>
2022-02-19 09:39:39 -0500

    add front end and game endpoint to index.php

--- index.php
+++ index.php
@@ -9,9 +9,13 @@ class testObject {}
 $myObj = new testObject;
 
 $myObj->name = "Data from the index.php endpoint of the Game Planning App API";
+$myObj->frontEnd = "https://gameplanning.site/";
 $myObj->endPointsDescription = "https://github.com/JacobStephens2/game-planning-back-end";
 $myObj->endPointBaseURL = "https://api.gameplanning.site/";
-$myObj->endPointsList = array("index.php", "database-test.php");
+$myObj->endPointsList = array(
+  "https://api.gameplanning.site/database-test.php", 
+  "https://api.gameplanning.site/game.php"
+);
 
 $myJSON = json_encode($myObj);
 
