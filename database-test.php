a4ca5559fb35788e4b9c46243f618603bccaee72
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-04-16 21:23:54 -0400

    remove files from old locations

--- database-test.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once('database/initialize.php');
-
-$site_data = array("value"=>get_site_data());
-
-echo json_encode($site_data);
-
-?>
--- game.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once('database/initialize.php');
-
-$data = array("value"=>get_game());
-
-echo json_encode($data);
-
-?>
