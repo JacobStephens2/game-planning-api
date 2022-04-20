91dce31d07a384c9f4883dd28caca3a5bf0f41cc
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-04-20 08:26:34 -0400

    add DatabaseObject class

---
+++ private/classes/databaseobject.class.php
@@ -0,0 +1,61 @@
+<?php
+
+class DatabaseObject {
+
+  static protected $database;
+  static protected $table_name = "";
+  static protected $columns = [];
+  public $errors = [];
+
+  static public function set_database($database) {
+    self::$database = $database;
+  }
+
+  static public function find_by_sql($sql) {
+    $result = static::$database->query($sql);
+    if(!$result) {
+      exit("Database query failed.");
+    }
+
+    // results into objects
+    $object_array = [];
+    while($record = $result->fetch_assoc()) {
+      $object_array[] = static::instantiate($record);
+    }
+
+    $result->free();
+
+    return $object_array;
+  }
+
+  static public function find_all() {
+    $sql = "SELECT * FROM " . static::$table_name;
+    return static::find_by_sql($sql);
+  }
+
+  static public function find_by_id($id) {
+    $sql = "SELECT * FROM " . static::$table_name . " ";
+    $sql .= "WHERE id='" . static::$database->escape_string($id) . "'";
+    $obj_array = static::find_by_sql($sql);
+    if(!empty($obj_array)) {
+      return array_shift($obj_array);
+    } else {
+      return false;
+    }
+  }
+
+  static protected function instantiate($record) {
+    $object = new static;
+    // Could manually assign values to properties
+    // but automatically assignment is easier and re-usable
+    foreach($record as $property => $value) {
+      if(property_exists($object, $property)) {
+        $object->$property = $value;
+      }
+    }
+    return $object;
+  }
+
+}
+
+?>
