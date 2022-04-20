bdda09c7f0535f6a15f8f7c6846fb927980c05ef
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-04-20 08:26:56 -0400

    make Game and User classes extend DatabaseObject

--- private/classes/game.class.php
+++ private/classes/game.class.php
@@ -1,19 +1,11 @@
 <?php
 
-class Game {
-
-  // ----- START OF ACTIVE RECORD CODE -----
-  static protected $database;
-  
-  static public function set_database($database) {
-    self::$database = $database;
-  }
+class Game extends DatabaseObject {
 
   static public function find_all() {
     $sql = "SELECT * FROM games";
     return self::$database->query($sql);
   }
-  // ----- END OF ACTIVE RECORD CODE -----
 
 }
 
--- private/classes/user.class.php
+++ private/classes/user.class.php
@@ -1,65 +1,17 @@
 <?php
 
-class User {
+class User extends DatabaseObject {
   
-  // ----- START OF ACTIVE RECORD CODE -----
-  static protected $database;
-
-  static public function set_database($database) {
-    self::$database = $database;
-  }
-
-    static public function find_by_sql($sql) {
-    $result = self::$database->query($sql);
-    if(!$result) {
-      exit("Database query failed.");
-    }
-
-    // results into objects
-    $object_array = [];
-    while($record = $result->fetch_assoc()) {
-      $object_array[] = self::instantiate($record);
-    }
-
-    $result->free();
-
-    return $object_array;
-  }
-
-  static public function find_all() {
-    $sql = "SELECT * FROM users";
-    return self::find_by_sql($sql);
-  }
-
-  static public function find_by_id($id) {
-    $sql = "SELECT * FROM users ";
-    $sql .= "WHERE id='" . self::$database->escape_string($id) . "'";
-    $obj_array = self::find_by_sql($sql);
-    if(!empty($obj_array)) {
-      return array_shift($obj_array);
-    } else {
-      return false;
-    }
-  }
-
-  static protected function instantiate($record) {
-    $object = new self;
-    // Could manually assign values to properties
-    // but automatically assignment is easier and re-usable
-    foreach($record as $property => $value) {
-      if(property_exists($object, $property)) {
-        $object->$property = $value;
-      }
-    }
-    return $object;
-  }
+  static protected $table_name = 'users';
+  static protected $db_columns = ['id', 'email', 'user_group', 'hashed_password'];
 
   public function create() {
     $sql = "INSERT INTO users (";
-    $sql .= "email, password ";
+    $sql .= "email, password, user_group ";
     $sql .= ") VALUES (";
     $sql .= "'" . $this->email . "', ";
     $sql .= "'" . $this->password . "'";
+    $sql .= "'1'";
     $sql .= ")";
     $result = self::$database->query($sql);
     if($result) {
@@ -67,7 +19,6 @@ class User {
     }
     return $result;
   }
-  // ----- END OF ACTIVE RECORD CODE -----
 
   public $id;
   public $email;
@@ -76,6 +27,10 @@ class User {
   public function __construct($data) {
     $this->email = $data->email;
     $this->password = $data->password;
+  }
+
+  public function verify_login_credentials() {
+
   }
 
 }
