1d1e5e04fb1b2f39de19f742aa239dd82a5d7416
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-04-20 08:27:39 -0400

    get login.php route to check user credentials

--- public/login.php
+++ public/login.php
@@ -3,33 +3,23 @@
 header('Access-Control-Allow-Credentials: true');
 
 // Takes raw data from the request
-// $json = file_get_contents('php://input');
+$json = file_get_contents('php://input');
 
-// $data = json_decode($json);
+$data = json_decode($json);
 
-// require_once('../private/initialize.php');
+require_once('../private/initialize.php');
 
-// $login_failure_msg = "Log in was unsuccessful.";
+if ($data->email == "") {
+  echo json_encode('Include an email address to log in');
+} else {
+  $user = new User($data);
+  $result = $user->verify_login_credentials($data);
+  if($result === true) {
+    echo json_encode('Log in successful');
+  } else {
+    echo json_encode('Failed to log in (result evaluated false)');
+  }
+}
 
-// $user = find_user_by_email($data->email);
-// if($user) {
-
-//   if(password_verify($password, $user['hashed_password'])) { // original
-//     // password matches
-//     log_in_user($user);
-//     redirect_to(url_for('/index.php'));      
-//   } else {
-//     // username found, but password does not match
-//     $errors[] = $login_failure_msg;
-//   }
-
-// } else {
-//   // no username found
-//   $errors[] = $login_failure_msg;
-// }
-
-// echo json_encode($data->email);
-
-echo 'This endpoint needs some work. Good luck logging in.'
 
 ?>
