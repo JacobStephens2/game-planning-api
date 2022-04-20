6ba7a709bc9adfc80d64bcccf54efcdbad75303f
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-04-20 07:49:42 -0400

    check if email in request is blank and do not process if yes

--- public/sign-up.php
+++ public/sign-up.php
@@ -9,13 +9,17 @@ $data = json_decode($json);
 
 require_once('../private/initialize.php');
 
-$user = new User($data);
-$result = $user->create();
-
-if($result === true) {
-  echo json_encode('User created');
+if ($data->email == "") {
+  echo json_encode('Include an email address to register');
 } else {
-  echo json_encode('Failed to create user (result evaluated false)');
+  $user = new User($data);
+  $result = $user->create();
+  if($result === true) {
+    echo json_encode('User created');
+  } else {
+    echo json_encode('Failed to create user (result evaluated false)');
+  }
 }
+
 
 ?>
