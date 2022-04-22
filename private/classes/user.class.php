abdf46133c9ee1f8d51ec1cd0f85dc1af9148ec7
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-04-22 09:32:13 -0400

    expand user verification

--- private/classes/user.class.php
+++ private/classes/user.class.php
@@ -5,35 +5,62 @@ class User extends DatabaseObject {
   static protected $table_name = 'users';
   static protected $db_columns = ['id', 'email', 'user_group', 'hashed_password'];
 
-  public function create() {
-    $sql = "INSERT INTO users (";
-    $sql .= "email, password, user_group ";
-    $sql .= ") VALUES (";
-    $sql .= "'" . $this->email . "', ";
-    $sql .= "'" . $this->password . "'";
-    $sql .= "'1'";
-    $sql .= ")";
-    $result = self::$database->query($sql);
-    if($result) {
-      $this->id = self::$database->insert_id;
+  public function create($email, $password) {
+    $email_registered = self::find_by_email($email);
+    if ($email_registered) {
+      return false;
+    } else {
+      $sql = "INSERT INTO users (";
+      $sql .= "email, password, user_group ";
+      $sql .= ") VALUES (";
+      $sql .= "'" . self::$database->escape_string($email) . "', ";
+      $sql .= "'" . self::$database->escape_string($password) . "', ";
+      $sql .= "'1'";
+      $sql .= ")";
+      $result = self::$database->query($sql);
+      if($result) {
+        $this->id = self::$database->insert_id;
+      }
+      return $result;
     }
-    return $result;
   }
 
   public $id;
   public $email;
   public $password;
 
-  public function __construct($data) {
-    $this->email = $data->email;
-    $this->password = $data->password;
-  }
-
   public function verify_login_credentials($email, $password) {
     $sql = "SELECT * FROM users ";
-    $sql .= "WHERE email = '$email' ";
-    $sql .= "AND password = '$password'";
-    return self::$database->query($sql);
+    $sql .= "WHERE email = '" . self::$database->escape_string($email) . "' ";
+    $sql .= "AND password = '" . self::$database->escape_string($password) . "'";
+    $obj_array = self::find_by_sql($sql);
+    if(!empty($obj_array)) {
+      return array_shift($obj_array);
+    } else {
+      return false;
+    }
+  }
+
+  static public function find_by_id($id) {
+    $sql = "SELECT * FROM users ";
+    $sql .= "WHERE id='" . self::$database->escape_string($id) . "'";
+    $obj_array = self::find_by_sql($sql);
+    if(!empty($obj_array)) {
+      return array_shift($obj_array);
+    } else {
+      return false;
+    }
+  }
+
+    static public function find_by_email($email) {
+    $sql = "SELECT * FROM users ";
+    $sql .= "WHERE email='" . self::$database->escape_string($email) . "'";
+    $obj_array = self::find_by_sql($sql);
+    if(!empty($obj_array)) {
+      return array_shift($obj_array);
+    } else {
+      return false;
+    }
   }
 
 }
