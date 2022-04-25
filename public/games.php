8dc09512c14568a1db027d9408f9fcf6ae070447
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-04-24 23:45:50 -0400

    generate JWT token at games.php

--- public/games.php
+++ public/games.php
@@ -9,16 +9,25 @@ require_once('../initialize.php');
 
 use Firebase\JWT\JWT;
 
-// extract credentials from the request
+// Generate token
+$secretKey  = $_ENV['JWTSECRET'];
+$issuedAt   = new DateTimeImmutable();
+$expire     = $issuedAt->modify('+6 minutes')->getTimestamp();      // Add 60 seconds
+$serverName = $_SERVER['SERVER_NAME'];
+// $username   = "username";                                           // Retrieved from filtered POST data
 
-// if ($hasValidCredentials) {
+$data = [
+    'iat'  => $issuedAt->getTimestamp(),         // Issued at: time when the token was generated
+    'iss'  => $serverName,                       // Issuer
+    'nbf'  => $issuedAt->getTimestamp(),         // Not before
+    'exp'  => $expire,                           // Expire
+    // 'userName' => $username,                     // User name
+];
 
-// }
-
-// Provide data
-
-$data = get_game();
-
-echo json_encode($data);
+echo JWT::encode(
+    $data,
+    $_ENV['JWTSECRET'],
+    'HS512'
+);
 
 ?>
