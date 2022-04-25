e9cb9a31f129c1e88ae004bfd30f692818255ce1
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-04-24 22:33:15 -0400

    add secrets for environment variables

--- private/initialize.php
+++ private/initialize.php
@@ -3,6 +3,7 @@
 include_once('database/database_functions.php');
 $database = db_connect();
 include_once('database/query_functions.php');
+require_once('secrets.php');
 
 require_once('classes/databaseobject.class.php');
 DatabaseObject::set_database($database);
@@ -10,6 +11,5 @@ DatabaseObject::set_database($database);
 // Classes that extend and inherit DatabaseObject
 require_once('classes/game.class.php');
 require_once('classes/user.class.php');
-
 
 ?>
