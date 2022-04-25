256bdcd5ccf0975135f645b49d8745ca284a8e64
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-04-24 23:27:55 -0400

    adjust file references in initialize for structure

--- initialize.php
+++ initialize.php
@@ -1,10 +1,10 @@
 <?php
 
-require_once('../vendor/autoload.php');
+require_once('vendor/autoload.php');
 
-include_once('database/database_functions.php');
+include_once('private/database/database_functions.php');
 $database = db_connect();
-include_once('database/query_functions.php');
+include_once('private/database/query_functions.php');
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
 $dotenv->load();
