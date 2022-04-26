86e6e76aa1fc240004489e395656d109c8c9accc
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-04-25 21:00:49 -0400

    send jwt access token in response to successful login

--- public/login.php
+++ public/login.php
@@ -1,13 +1,17 @@
 <?php
 
+declare(strict_types=1);
+
+require_once('../initialize.php');
+
+use Firebase\JWT\JWT;
+
 header('Access-Control-Allow-Credentials: true');
 
 // Takes raw data from the request
 $json = file_get_contents('php://input');
 
 $data = json_decode($json);
-
-require_once('../initialize.php');
 
 $response = new stdClass();
 
@@ -18,8 +22,34 @@ if ($data->email == "") {
   $user = new User();
   $login_result = $user->verify_login_credentials( $data->email, $data->password );
   if( $login_result ) {
+
     $response->message = 'Log in succeeded';
+
+    // Validate JWT access token sent with request 
+    // before responding with the game data
+    // Generate token
+    $secretKey  = $_ENV['JWTSECRET'];
+    $issuedAt   = new DateTimeImmutable();
+    $expire     = $issuedAt->modify('+6 minutes')->getTimestamp();      // Add 60 seconds
+    $serverName = $_SERVER['SERVER_NAME'];
+    // $username   = "username";                                           // Retrieved from filtered POST data
+
+    $data = [
+        'iat'  => $issuedAt->getTimestamp(),         // Issued at: time when the token was generated
+        'iss'  => $serverName,                       // Issuer
+        'nbf'  => $issuedAt->getTimestamp(),         // Not before
+        'exp'  => $expire,                           // Expire
+        // 'userName' => $username,                     // User name
+    ];
+
+    $response->jwt = JWT::encode(
+        $data,
+        $_ENV['JWTSECRET'],
+        'HS512'
+    );
+
     echo json_encode($response);
+    
   } else {
     $response->message = 'Log in failed';
     echo json_encode($response);
