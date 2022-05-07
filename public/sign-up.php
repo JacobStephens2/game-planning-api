20a6ca2ed632b649246344df905814afa543078c
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-05-07 16:28:19 -0400

    send json object in response on /sign-up

--- public/sign-up.php
+++ public/sign-up.php
@@ -11,15 +11,20 @@ $data = json_decode($json);
 
 require_once('../initialize.php');
 
+$response = new stdClass();
+
 if ($data->email == "") {
-  echo json_encode('Submit an email address to register');
+  $response->message = 'Submit an email address to register';
+  echo json_encode($response);
 } else {
   $user = new User($data);
   $result = $user->create( $data->email, $data->password );
   if( $result ) {
-    echo json_encode('You have signed up');
+    $response->message = 'Log in succeeded';
+    echo json_encode($response);
   } else {
-    echo json_encode('An error occured in sign up');
+    $response->message = 'An error occured in sign up';
+    echo json_encode($response);
   }
 }
 
