f7ee3d63922e7d67b3957a51792e135f01e2843e
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-05-07 00:28:45 -0400

    verify access token jwt on /games

--- public/games.php
+++ public/games.php
@@ -2,4 +2,35 @@
 
 // Require valid access token before giving access to resource
 
+declare(strict_types=1);
+
+require_once('../initialize.php');
+
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+$key  = $_ENV['JWT_SECRET'];
+
+$jwt = $_COOKIE["access_token"];
+
+/**
+ * IMPORTANT:
+ * You must specify supported algorithms for your application. See
+ * https://tools.ietf.org/html/draft-ietf-jose-json-web-algorithms-40
+ * for a list of spec-compliant algorithms.
+ */
+
+if ( $decoded = JWT::decode($jwt, new Key($key, 'HS512')) ) {
+
+  // echo a json encoded version of the decoded JWT if user authenticated
+  // authenticated meaning the access token signature has been verified
+  echo json_encode($decoded);
+
+} else {
+
+  echo json_encode('You are not authenticated');
+
+}
+
+
 ?>
