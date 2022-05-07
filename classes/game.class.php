63cbafb5bdb1e5bb73e40469c15017455159bcad
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-05-07 15:02:35 -0400

    add /games/read

--- classes/game.class.php
+++ classes/game.class.php
@@ -26,21 +26,50 @@ class Game {
     return $result;
   }
 
-  public $title;
+
 
   static public function find_by_sql($sql) {
-    return self::$database->query($sql);
+    $result = self::$database->query($sql);
     if(!$result) {
       exit('Database query failed.');
     }
-    return $result;
+
+    // results into objects
+    $object_array = [];
+    while ($record = $result->fetch_assoc()) {
+      $object_array[] = self::instantiate($record);
+    }
+
+    $result->free();
+    
+    return $object_array;
   }
-  
+
+
+
+  static protected function instantiate($record) {
+    $object = new self;
+    // could manually assign values to properties
+    // but automatic assignment is easier and re-usable
+    foreach($record as $property => $value) {
+      if(property_exists($object, $property)) {
+        $object->$property = $value;
+      }
+    }
+    return $object;
+  }
+
+
+
+  public $id;
+  public $title;
+
+
+
   static public function find_all() {
     $sql = "SELECT * FROM games_test";
     return self::find_by_sql($sql);
   }
-
 
 }
 
--- public/games/read.php
+++ public/games/read.php
@@ -2,19 +2,11 @@
 
 require('../../initialize.php');
 
-$game = new Game();
-$result = $game->find_all();
+// Get array back from Game::find_all()
+$games = Game::find_all();
 
-echo '$result: <pre>';
-print_r($result);
-echo '</pre>';
-
-$row = $result->fetch_assoc();
-
-echo '$row: <pre>';
-print_r($row);
-echo '</pre>';
-
-$result->free();
+foreach($games as $game) {
+  echo '<li>' . $game->title . '</li>';
+}
 
 ?>
