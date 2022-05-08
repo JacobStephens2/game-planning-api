c4b7e6006e6d40d224cd57bed22156d72b5ff0a7
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-05-08 16:25:43 -0400

    check if cookie sent to /test/access

--- public/test/access.php
+++ public/test/access.php
@@ -13,28 +13,25 @@ use Firebase\JWT\Key;
 
 $key  = $_ENV['JWT_SECRET'];
 
-$jwt = $_COOKIE["access_token"];
-
-header('Access-Control-Allow-Credentials: true');
-
-/**
- * IMPORTANT:
- * You must specify supported algorithms for your application. See
- * https://tools.ietf.org/html/draft-ietf-jose-json-web-algorithms-40
- * for a list of spec-compliant algorithms.
- */
-
-if ( $decoded = JWT::decode($jwt, new Key($key, 'HS512')) ) {
-
-  // echo a json encoded version of the decoded JWT if user authenticated
-  // authenticated meaning the access token signature has been verified
-  echo json_encode($decoded);
+if (empty($_COOKIE["access_token"])) {
+  
+  $response = new stdClass();
+  $response->message = 'You are not authenticated';
+  echo json_encode($response);
 
 } else {
+  
+  $jwt = $_COOKIE["access_token"];
+  // echo a json encoded version of the decoded JWT if user authenticated
+  // authenticated meaning the access token signature has been verified
 
-  echo json_encode('You are not authenticated');
+  // IMPORTANT:
+  // You must specify supported algorithms for your application. See
+  // https://tools.ietf.org/html/draft-ietf-jose-json-web-algorithms-40
+  // for a list of spec-compliant algorithms.
+
+  echo json_encode(JWT::decode($jwt, new Key($key, 'HS512')));
 
 }
-
 
 ?>
