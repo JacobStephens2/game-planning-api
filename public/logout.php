366acbf8dbd7c6427a9d1a4e390c64ed6421feaf
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-05-08 22:37:55 -0400

    add /logout

---
+++ public/logout.php
@@ -0,0 +1,30 @@
+<?php
+
+declare(strict_types=1);
+
+error_reporting(0);
+
+require_once('../initialize.php');
+
+header('Access-Control-Allow-Credentials: true');
+
+// Takes raw data from the request
+$json = file_get_contents('php://input');
+
+$response = new stdClass();
+      
+setcookie(
+  "access_token", // name
+  "", // value
+  time() + (86400 * 7), // expire, 86400 = 1 day
+  "", // path
+  $_ENV['COOKIE_DOMAIN'], // domain
+  $cookie_secure, // secure
+  true // httponly
+); 
+
+$response->message = 'Logged out';
+
+echo json_encode($response);
+
+?>
