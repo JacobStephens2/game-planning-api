ee90ade456a37297c8727eff6f7dd19be63c2ba5
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-05-09 02:14:09 -0400

    remove cookie check and just check token

--- authenticate.php
+++ authenticate.php
@@ -7,21 +7,16 @@ use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
 
 function authenticate() {
-  $response = new stdClass();
-  if (empty($_COOKIE["access_token"])) {
-    $response->message = 'You are not authenticated';
+  try {
+    $jwt = $_COOKIE["access_token"];
+    $key  = $_ENV['JWT_SECRET'];
+    $decodedJWT = JWT::decode($jwt, new Key($key, 'HS512'));
+  } catch (Exception $e) {
+    $response = new stdClass();
+    $response->message = 'You have not been authenticated';
+    $response->exception = 'Caught exception: ' . $e->getMessage();
     echo json_encode($response);
-  } else {
-    try {
-      $jwt = $_COOKIE["access_token"];
-      $key  = $_ENV['JWT_SECRET'];
-      $decodedJWT = JWT::decode($jwt, new Key($key, 'HS512'));
-    } catch (Exception $e) {
-      $response->message = 'You have not been authenticated';
-      $response->exception = 'Caught exception: ' . $e->getMessage();
-      echo json_encode($response);
-      exit;
-    }
+    exit;
   }
 }
 
