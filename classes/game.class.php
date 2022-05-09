d15e8c60382eba433301a1cb6690efda9a332c3e
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-05-08 20:59:00 -0400

    change merge_attributes($args) to ($args=[])

--- classes/game.class.php
+++ classes/game.class.php
@@ -2,20 +2,57 @@
 
 class Game {
 
-  //  Start of Active Record Code
+  // ----- START OF ACTIVE RECORD CODE ------
   static protected $database;
-
   static protected $db_columns = ['id', 'title'];
-
-  public $title;
 
   static public function set_database($database) {
     self::$database = $database;
   }
-  // End of Active Record Code
- 
-  public function __construct($args=[]) {
-    $this->title = $args['title'] ?? NULL;
+
+  static public function find_by_sql($sql) {
+    $result = self::$database->query($sql);
+    if(!$result) {
+      exit("Database query failed.");
+    }
+
+    // results into objects
+    $object_array = [];
+    while($record = $result->fetch_assoc()) {
+      $object_array[] = self::instantiate($record);
+    }
+
+    $result->free();
+
+    return $object_array;
+  }
+
+  static public function find_all() {
+    $sql = "SELECT * FROM games_test";
+    return self::find_by_sql($sql);
+  }
+
+  static public function find_by_id($id) {
+    $sql = "SELECT * FROM games_test ";
+    $sql .= "WHERE id='" . self::$database->escape_string($id) . "'";
+    $obj_array = self::find_by_sql($sql);
+    if(!empty($obj_array)) {
+      return array_shift($obj_array);
+    } else {
+      return false;
+    }
+  }
+
+  static protected function instantiate($record) {
+    $object = new self;
+    // Could manually assign values to properties
+    // but automatically assignment is easier and re-usable
+    foreach($record as $property => $value) {
+      if(property_exists($object, $property)) {
+        $object->$property = $value;
+      }
+    }
+    return $object;
   }
 
   protected function create() {
@@ -38,6 +75,7 @@ class Game {
     foreach($attributes as $key => $value) {
       $attribute_pairs[] = "{$key}='{$value}'";
     }
+    
     $sql = "UPDATE games_test SET ";
     $sql .= join(', ', $attribute_pairs);
     $sql .= " WHERE id='" . self::$database->escape_string($this->id) . "' ";
@@ -47,7 +85,7 @@ class Game {
   }
 
   public function save() {
-    // A new record will not yet have an id
+    // A new record will not have an ID yet
     if(isset($this->id)) {
       return $this->update();
     } else {
@@ -55,12 +93,22 @@ class Game {
     }
   }
 
-  public function merge_attributes($args) {
+  public function merge_attributes($args=[]) {
     foreach($args as $key => $value) {
       if(property_exists($this, $key) && !is_null($value)) {
         $this->$key = $value;
       }
     }
+  }
+
+  // Properties which have database columns, excluding ID
+  public function attributes() {
+    $attributes = [];
+    foreach(self::$db_columns as $column) {
+      if($column == 'id') { continue; }
+      $attributes[$column] = $this->$column;
+    }
+    return $attributes;
   }
 
   protected function sanitized_attributes() {
@@ -71,59 +119,13 @@ class Game {
     return $sanitized;
   }
 
-  // Properties which have database columns, excluding id
-  public function attributes() {
-    $attributes = [];
-    foreach(self::$db_columns as $column) {
-      if($column == 'id') { continue; }
-      $attributes[$column] = $this->$column;
-    }
-    return $attributes;
-  }
+  // ----- END OF ACTIVE RECORD CODE ------
 
-  static public function find_by_sql($sql) {
-    $result = self::$database->query($sql);
-    if(!$result) {
-      exit('Database query failed.');
-    }
-
-    // results into objects
-    $object_array = [];
-    while ($record = $result->fetch_assoc()) {
-      $object_array[] = self::instantiate($record);
-    }
-
-    $result->free();
-    
-    return $object_array;
-  }
-
-  static protected function instantiate($record) {
-    $object = new self;
-    // could manually assign values to properties
-    // but automatic assignment is easier and re-usable
-    foreach($record as $property => $value) {
-      if(property_exists($object, $property)) {
-        $object->$property = $value;
-      }
-    }
-    return $object;
-  }
-
-  static public function find_all() {
-    $sql = "SELECT * FROM games_test";
-    return self::find_by_sql($sql);
-  }
-
-  static public function find_by_id($id) {
-    $sql = "SELECT * FROM games_test ";
-    $sql .= "WHERE id = '" . self::$database->escape_string($id) . "'";
-    $obj_array = self::find_by_sql($sql);
-    if(!empty($obj_array)) {
-      return array_shift($obj_array);
-    } else {
-      return false;
-    }
+  public $id;
+  public $title;
+ 
+  public function __construct($args=[]) {
+    $this->title = $args['title'] ?? NULL;
   }
 
 }
