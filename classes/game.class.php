b8f7c2149eb28f726fc4567936f63bdffded7a9e
Jacob Stephens <jacob.stephens.701@gmail.com>
2022-05-11 22:39:16 -0400

    make dbobject class from game class

--- classes/game.class.php
+++ classes/game.class.php
@@ -1,59 +1,15 @@
 <?php
 
-class Game {
+class Game extends DBObject {
 
-  // ----- START OF ACTIVE RECORD CODE ------
-  static protected $database;
+  static protected $table_name = 'games_test';
   static protected $db_columns = ['id', 'title'];
-  public $errors = [];
 
-  static public function set_database($database) {
-    self::$database = $database;
-  }
-
-  static public function find_by_sql($sql) {
-    $result = self::$database->query($sql);
-    if(!$result) {
-      exit("Database query failed.");
-    }
-
-    // results into objects
-    $object_array = [];
-    while($record = $result->fetch_assoc()) {
-      $object_array[] = self::instantiate($record);
-    }
-
-    $result->free();
-
-    return $object_array;
-  }
-
-  static public function find_all() {
-    $sql = "SELECT * FROM games_test";
-    return self::find_by_sql($sql);
-  }
-
-  static public function find_by_id($id) {
-    $sql = "SELECT * FROM games_test ";
-    $sql .= "WHERE id='" . self::$database->escape_string($id) . "'";
-    $obj_array = self::find_by_sql($sql);
-    if(!empty($obj_array)) {
-      return array_shift($obj_array);
-    } else {
-      return false;
-    }
-  }
-
-  static protected function instantiate($record) {
-    $object = new self;
-    // Could manually assign values to properties
-    // but automatically assignment is easier and re-usable
-    foreach($record as $property => $value) {
-      if(property_exists($object, $property)) {
-        $object->$property = $value;
-      }
-    }
-    return $object;
+  public $id;
+  public $title;
+ 
+  public function __construct($args=[]) {
+    $this->title = $args['title'] ?? NULL;
   }
 
   protected function validate() {
@@ -64,100 +20,6 @@ class Game {
     }
 
     return $this->errors;
-  }
-
-  protected function create() {
-    $this->validate();
-    if(!empty($this->errors)) { return false; }
-
-    $attributes = $this->sanitized_attributes();
-    $sql = "INSERT INTO games_test (";
-    $sql .= join(', ', array_keys($attributes));
-    $sql .= ") VALUES ('";
-    $sql .= join("', '", array_values($attributes));
-    $sql .= "')";
-    $result = self::$database->query($sql);
-    if($result) {
-      $this->id = self::$database->insert_id;
-    }
-    return $result;
-  }
-
-  protected function update() {
-    $this->validate();
-    if(!empty($this->errors)) { return false; }
-
-    $attributes = $this->sanitized_attributes();
-    $attribute_pairs = [];
-    foreach($attributes as $key => $value) {
-      $attribute_pairs[] = "{$key}='{$value}'";
-    }
-    
-    $sql = "UPDATE games_test SET ";
-    $sql .= join(', ', $attribute_pairs);
-    $sql .= " WHERE id='" . self::$database->escape_string($this->id) . "' ";
-    $sql .= "LIMIT 1";
-    $result = self::$database->query($sql);
-    return $result;
-  }
-
-  public function save() {
-    // A new record will not have an ID yet
-    if(isset($this->id)) {
-      return $this->update();
-    } else {
-      return $this->create();
-    }
-  }
-
-  public function merge_attributes($args=[]) {
-    foreach($args as $key => $value) {
-      if(property_exists($this, $key) && !is_null($value)) {
-        $this->$key = $value;
-      }
-    }
-  }
-
-  // Properties which have database columns, excluding ID
-  public function attributes() {
-    $attributes = [];
-    foreach(self::$db_columns as $column) {
-      if($column == 'id') { continue; }
-      $attributes[$column] = $this->$column;
-    }
-    return $attributes;
-  }
-
-  protected function sanitized_attributes() {
-    $sanitized = [];
-    foreach($this->attributes() as $key => $value) {
-      $sanitized[$key] = self::$database->escape_string($value);
-    }
-    return $sanitized;
-  }
-
-  public function delete() {
-    $sql = "DELETE FROM games_test ";
-    $sql .= "WHERE id='" . self::$database->escape_string($this->id) . "' ";
-    $sql .= "LIMIT 1";
-    $result = self::$database->query($sql);
-    return $result;
-
-    // After deleting, the instance of the object will still
-    // exist, even though the database record does not.
-    // This can be useful, as in:
-    //   echo $user->first_name . " was deleted.";
-    // but, for example, we can't call $user->update() after
-    // calling $user->delete().
-  }
-
-  // ----- END OF ACTIVE RECORD CODE ------
-
-  public $id;
-  public $title;
- 
-  public function __construct($args=[]) {
-    $this->title = $args['title'] ?? NULL;
   }
 
 }
--- initialize.php
+++ initialize.php
@@ -11,8 +11,11 @@ include_once('database/database_functions.php');
 $database = db_connect();
 include_once('database/query_functions.php');
 
+require_once('classes/dbobject.class.php');
+DBObject::set_database($database);
+
+// Classes that extend DBObject
 require_once('classes/game.class.php');
-Game::set_database($database);
 
 require_once('classes/databaseobject.class.php');
 DatabaseObject::set_database($database);
